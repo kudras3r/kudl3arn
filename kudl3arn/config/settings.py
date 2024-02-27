8189eaa43f3dc4d7e841d3f1420e79e200f14b20
kudras3r <d8880888@mail.ru>
2024-02-28 00:55:05 +0300

    adding index page

--- kudl3arn/config/settings.py
+++ kudl3arn/config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', BASE_DIR / '../')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'src.users',
+    'users',
+
 ]
 
 MIDDLEWARE = [
--- kudl3arn/users/urls.py
+++ kudl3arn/users/urls.py
@@ -4,9 +4,8 @@ URL configuration for kudl3arn project.
 from django.contrib import admin
 from django.urls import path
 
-from users.views import index
+from views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', index, name='index'),
 ]
