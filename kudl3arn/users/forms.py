c51480aa636c0a014cb463ba168b054206857585
kudras3r <d8880888@mail.ru>
2024-03-05 01:53:03 +0300

    login page refactor

--- kudl3arn/users/forms.py
+++ kudl3arn/users/forms.py
@@ -1,9 +1,23 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django import forms
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'text-field_login_input',
+        'name': 'login',
+        'id': 'login',
+        'placeholder': 'Username or Email'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'text-field_pwd_input',
+        'name': 'password',
+        'id': 'password',
+        'placeholder': 'Password'
+    }))
+
     class Meta:
         model = User
         fields = ('username', 'password')
